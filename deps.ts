0948d721e4d5d7a69df27cf6218260909ae65e97
yo-goto <50942816+yo-goto@users.noreply.github.com>
2023-07-29 19:30:36 +0900

    ✨feature: リポジトリをdeno運用に変更

---
+++ deps.ts
@@ -0,0 +1,6 @@
+import "npm:zenn-cli@^0.1.144";
+import "npm:textlint@^13.3.3";
+import "npm:textlint-rule-preset-ja-spacing@^2.3.0";
+import "npm:textlint-rule-preset-jtf-style@2.3.13";
+import "npm:textlint-rule-preset-ja-technical-writing@^8.0.0";
+import "npm:@proofdict/textlint-rule-proofdict@^3.1.2";
