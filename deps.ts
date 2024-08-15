4d79b489a341d87a46a945f112f5146f64d2a75f
yo-goto <50942816+yo-goto@users.noreply.github.com>
2024-08-15 18:46:46 +0900

    🔵 chore(deps): markdownlint-cli2を追加

--- deps.ts
+++ deps.ts
@@ -5,3 +5,4 @@ import "npm:textlint-rule-preset-ja-spacing@^2.4.3";
 import "npm:textlint-rule-preset-jtf-style@2.3.14";
 import "npm:textlint-rule-preset-ja-technical-writing@^10.0.0";
 import "npm:@proofdict/textlint-rule-proofdict@^3.1.2";
+import "npm:markdownlint-cli2@^0.13.0";
